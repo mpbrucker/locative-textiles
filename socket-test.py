31ae5d09461ee71e2fd6c19b9bea71d549dbdfff
Matt Brucker <mpbrucker@gmail.com>
2021-05-17 20:50:23 -0500

    Getting CV async notification working.

--- socket-test.py
+++ socket-test.py
@@ -8,7 +8,6 @@ import websockets
 async def hello():
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
-        name = input("What's your name? ")
 
         greeting = await websocket.recv()
         print(f"< {greeting}")
